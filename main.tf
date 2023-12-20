c874032fbe4d28089553c89215ab82379d9b3ec6
ykittipak <kittipak.y@gmail.com>
2023-12-20 17:42:51 +0700

    Chage VPC environment

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
+ cidr_block = "20.0.0.0/16"
  
  tags = {
-   Name = "Project VPC"
+   Name = "Project VPC2"
  }
 }
 resource "aws_subnet" "public_subnets" {
